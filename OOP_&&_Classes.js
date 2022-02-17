027e7fab98fb15cca64c9fbbe0a08dd75d99c10d
Anthony Malary <amalary@bu.edu>
2022-02-17 13:34:28 -0800

    Adding constructor function to our class

--- OOP_&&_Classes.js
+++ OOP_&&_Classes.js
@@ -30,6 +30,53 @@ class Person {
 
 const me = new Person(); 
 
-me.greeting('Jack')
+me.greeting('Jack'); 
 
 
+class Person {
+    greeting(character){
+        console.log(`Hello ${character} how's everything been going with you ?`);
+    }
+    jump(){
+        console.log('weeeee!');
+    }
+};
+
+const me = new Person(); 
+
+me.greeting('Bradly')
+me.jump(); 
+
+class Person {
+    greet(otherPerson){
+        console.log('hi ' + otherPerson + '!'); }
+        
+        jump(){
+            console.log('weeee!'); }
+};
+
+const me = new Person(); console.log(me);
+
+
+console.log(Person); 
+
+class Person {
+
+    constructor(){
+        this.legs = 2; 
+        this.arms = 2; 
+        this.eyes = 'Brown'; 
+        this.hair = 'Black'; 
+    }
+    greeting(character){
+        console.log(`Hello ${character} how is it going toda ?`)
+    }
+    jump(){
+        console.log('weeee!')
+    }
+    
+}; 
+
+const me = new Person();
+
+console.log(me);
