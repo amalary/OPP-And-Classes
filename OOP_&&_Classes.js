038b6764c270763257e11d8510a1650c9bb66d18
Anthony Malary <amalary@bu.edu>
2022-03-07 15:42:02 -0800

    Make factories

--- OOP_&&_Classes.js
+++ OOP_&&_Classes.js
@@ -265,3 +265,95 @@ const superman = new SuperHero ('Clark',23,'Blue', 'Black');
 console.log(superman); 
 
 console.log(superman.superPowers)
+
+class Person { 
+    static eyeColors(){
+        return ['blue','green','brown']
+    }
+    constructor (name,age, eyes, hair){
+        this.legs = 2;
+        this.arms = 2; 
+        this.name = name; 
+        this.age = age; 
+        this.eyes = eyes; 
+        this.hair = hair; 
+    }
+    setHairColor(haircolor){
+        this.hair = haircolor; 
+    }
+    greet(otherperson){
+        console.log('hi ' + otherperson + ' !');
+    }
+    jump(){
+    
+        console.log('weee!');
+}
+};
+
+class SuperHero extends Person {
+    fly(){
+        console.log('Up Up and Away !')
+    }
+    greet(otherperson){
+        console.log('Greetings ' + otherperson + ' !')
+    }
+};
+
+const superman = new SuperHero('Clark Kent', 30, Person.eyeColors()[0], 'black');
+
+console.log(superman); 
+
+// Create a Factory 
+
+class Car { 
+    constructor(serialNumber){
+        this.serialNumber = serialNumber; 
+    }
+    drive(){
+        console.log('Vroom');
+    }
+}
+
+const factory = { 
+    cars: [],
+    generateCar(){
+        const newCar = new Car(this.cars.length);
+        this.cars.push(newCar); 
+        return newCar; 
+    }, 
+    findCarIndex(index){
+        return this.cars[index];
+    }
+}
+
+factory.generateCar();
+factory.generateCar(); 
+console.log(factory);
+console.log(factory.findCarIndex(1));
+
+class Car {
+    constructor (maker,serialNumber){
+        this.maker= maker ; 
+        this.serialNumber = serialNumber;
+    }
+    drive(){
+        console.log('Vroom')
+    }
+}
+
+class Factory {
+    constructor (company) {
+        this.company = company; 
+        this.cars = [];
+    }
+    generateCar() {
+        const newCar = new Car(this.company, this.cars.length)
+        this.cars.push(newCar); 
+        return newCar
+    }
+    findCarIndex(index){
+        return this.cars[index]; 
+    }
+}
+
+const toyota = new Factory("Toyota"); const bmw = new Factory("BMW"); toyota.generateCar(); bmw.generateCar(); console.log(bmw.findCarIndex(0)); console.log(toyota.findCarIndex(0));
